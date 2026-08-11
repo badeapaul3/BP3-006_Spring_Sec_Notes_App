63119458e54bc3234f22f61678946ddac9e2e42e
Paul <badeapaul3@gmail.com>
2026-08-11 14:47:08 +0300

    First commit

---
+++ src/main/java/com/demo/spring_sec_notes_app/SpringSecNotesAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring_sec_notes_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecNotesAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecNotesAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/spring_sec_notes_app/SpringSecNotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring_sec_notes_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecNotesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
